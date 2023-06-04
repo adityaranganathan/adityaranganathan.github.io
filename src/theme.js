d889859bc73a1e43b34fa21638c8d7b43d53ba17
Aditya Ranganathan <aditya.ranganath68@gmail.com>
2023-06-04 15:59:01 +0530

    Add title and favicon

--- src/theme.js
+++ src/theme.js
@@ -3,11 +3,11 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   fonts: {
     heading: `'Lora Variable', sans-serif`,
-    body: `'Lexend Variable', sans-serif`,
+    body:    `'Lexend Variable', sans-serif`,
   },
   colors: {
     brand: {
-        0:"#F7F3ED",
+      0: "#F7F3ED",
       1: "#F5EFE7",
       2: "#D8C4B6",
       3: "#4F709C",
